6613a7868628e4b2426b040fee8d9ce22254ed92
Haohan Zhang <edwardzh@usc.edu>
2023-03-29 20:59:57 -0700

    first part completed

--- wordle.cpp
+++ wordle.cpp
@@ -21,7 +21,59 @@ std::set<std::string> wordle(
     const std::string& floating,
     const std::set<std::string>& dict)
 {
-    // Add your code here
+    //find which letters are fixed
+    std::map<int,char> fixedLetters;//this data structure stores the fixed letters and their positions
+    for(size_t i = 0; i < in.length(); i++){
+        if(in[i] != '-'){
+            fixedLetters.insert(std::make_pair(i,in[i]));
+        }
+    }
+    std::set<std::string> result;
+    std::set<std::string>::iterator it;
+    for(it = dict.begin(); it != dict.end(); ++it){
+        if(it->length() == in.length()){//size matches
+            //fixed letter check
+            bool fixedLetterCheck = true;
+            bool floatingLetterCheck = true;
+            bool upperCaseCheck = true;
+            std::map<int,char>::iterator it2;
+            //check if upper case letter exist
+            for(size_t i = 0; i < it->length(); i++){
+                if(isupper(it->at(i))){
+                    upperCaseCheck = false;
+                    break;
+                }
+            }
+            if(!upperCaseCheck)
+                continue;
+            for(it2 = fixedLetters.begin(); it2 != fixedLetters.end(); ++it2){
+                if(it->at(it2->first) != it2->second){
+                    fixedLetterCheck = false;
+                    break;
+                }
+            }
+            if(!fixedLetterCheck)
+                continue;
+            else{
+                //floating letter check
+                string tempWord = *it;
+                for(size_t i = 0; i<floating.length(); ++i){
+                    int index = tempWord.find(floating[i]);
+
+                    if(tempWord.find(floating[i]) == std::string::npos){
+                        floatingLetterCheck = false;
+                        break;
+                    }
+                    else{
+                        tempWord.at(index) = ' ';
+                    }
+                }
+                if(floatingLetterCheck)
+                    result.insert(*it);
+            }
+        }
+    }
+    return result;
 
 }
 
